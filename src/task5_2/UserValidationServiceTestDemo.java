a8bf4d4dcd7c04efb9db821f390e8ffad1ada93f
Lina Shevkunova <linamars2014@gmail.com>
2021-01-19 03:11:09 +0300

    User validation service is done

---
+++ src/task5_2/UserValidationServiceTestDemo.java
@@ -0,0 +1,20 @@
+package task5_2;
+
+public class UserValidationServiceTestDemo {
+    public static void main(String[] args) {
+        System.out.println("Test results:");
+        UserValidationServiceTest.minAgeTest();
+        System.out.println("..............");
+        UserValidationServiceTest.maxAgeTest();
+        System.out.println("..............");
+        UserValidationServiceTest.shortFirstNameTest();
+        System.out.println("..............");
+        UserValidationServiceTest.longFirstNameTest();
+        System.out.println("..............");
+        UserValidationServiceTest.shortLastNameTest();
+        System.out.println("..............");
+        UserValidationServiceTest.longLastNameTest();
+        System.out.println("..............");
+
+    }
+}
